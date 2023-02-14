62e9d615a3b58009bbee0c6d61a71f8176d7fb21
Howard Coffin <howard2706@gmail.com>
2023-02-14 00:28:07 -0800

    working robot

--- IMU.cpp
+++ IMU.cpp
@@ -2,6 +2,7 @@ extern "C" {
 	#include <inttypes.h>
 }
 
+#include <Wire.h>
 #include <math.h>
 #include "IMU.h"
 
@@ -12,21 +13,23 @@ float reading_to_float(uint8_t lower_byte, uint8_t higher_byte, float factor=1) 
 	return ((float)intval) * factor;
 }
 
-void IMU::init_sensors(float local_g_acc) {
-	// accelerometer init
+void IMU::init_accelerometer(float local_g_acc, uint8_t mode) {
 	Wire.beginTransmission(ACC_I2C_ADD);
 	Wire.write(ACC_CTRL_REG1_A);
-	Wire.write((ACC_DATA_RATE_1344_5376HZ << 4) | (ACC_LOW_POWER_MODE << 3) | 0b111); // 0b111 enables xyz axes
+	Wire.write((ACC_DATA_RATE_1344_5376HZ << 4) | (mode << 3) | 0b111); // 0b111 enables xyz axes
 	Wire.endTransmission();
 
 	Wire.beginTransmission(ACC_I2C_ADD);
 	Wire.write(ACC_CTRL_REG4_A);
-	Wire.write((ACC_SCALE_2g << 4) | ((1-ACC_LOW_POWER_MODE) << 3)); // 1 - mode is used to invert mode
+	Wire.write((ACC_SCALE_2g << 4) | ((1-mode) << 3)); // 1 - mode is used to invert mode
 	Wire.endTransmission();
 	acc_factor = 1 / (pow(2.0, 15.0) / (2.0 * local_g_acc));
 
+	acc_status = 0;
+	read_accelerometer();
+}
 
-	// gyro init
+void IMU::init_gyro() {
 	Wire.beginTransmission(GYR_I2C_ADD);
 	Wire.write(GYR_CTRL_REG1);
 	Wire.write((GYR_DATA_RATE_800HZ << 6) | (GYR_LPF2_CUTOFF_110HZ << 4) | 0b1111); // 0b1111 turns on and enables xyz axes
@@ -37,12 +40,22 @@ void IMU::init_sensors(float local_g_acc) {
 	Wire.write(GYR_SCALE_250DPS << 4);
 	Wire.endTransmission();
 	gyro_factor = 1 / (pow(2.0, 15.0) / (250.0));
+
+	gyr_status = 0;
+	read_gyro();
+}
+
+IMU::IMU(float local_g_acc) {
+	Wire.begin();
+	Wire.setClock(400000);
+	init_accelerometer(local_g_acc, ACC_NORMAL_POWER_MODE);
+	init_gyro();
 }
 
 bool IMU::read_accelerometer() {
 	uint8_t bytes_read = Wire.requestFrom(ACC_I2C_ADD, 7, ACC_STATUS_REG_A | (1 << 7), 1, true);
-	uint8_t acc_status = Wire.read();
-	if (acc_status & (1 << 3)) {
+	acc_status = acc_status | Wire.read();
+	if ((uint8_t)(acc_status << 5) == 0b11100000) {
 		// new data available for accelerometer
 		uint8_t xl = Wire.read();
 		uint8_t xh = Wire.read();
@@ -51,9 +64,11 @@ bool IMU::read_accelerometer() {
 		uint8_t zl = Wire.read();
 		uint8_t zh = Wire.read();
 			
-		xa = reading_to_float(xl, xh, acc_factor);
-		ya = reading_to_float(yl, yh, acc_factor);
-		za = reading_to_float(zl, zh, acc_factor);
+		xa = -reading_to_float(xl, xh, acc_factor);
+		ya = -reading_to_float(yl, yh, acc_factor);
+		za = -reading_to_float(zl, zh, acc_factor);
+
+		acc_status = 0;
 		return true;
 	} else {
 		return false;
@@ -61,10 +76,9 @@ bool IMU::read_accelerometer() {
 }
 
 bool IMU::read_gyro() {
-	long t_start = micros();
 	uint8_t bytes_read = Wire.requestFrom(GYR_I2C_ADD, 7, GYR_STATUS_REG | (1 << 7), 1, true);
-	uint8_t gyr_status = Wire.read();
-	if (gyr_status & (1 << 3)) {
+	gyr_status = gyr_status | Wire.read();
+	if ((uint8_t)(gyr_status << 5) == 0b11100000) {
 		// new data available for gyro
 		uint8_t xl = Wire.read();
 		uint8_t xh = Wire.read();
@@ -76,8 +90,11 @@ bool IMU::read_gyro() {
 		xw = reading_to_float(xl, xh, gyro_factor);
 		yw = reading_to_float(yl, yh, gyro_factor);
 		zw = reading_to_float(zl, zh, gyro_factor);
+
+		gyr_status = 0;
 		return true;
 	} else {
+		Serial.println(gyr_status, BIN);
 		return false;
 	}
 }
--- IMU.h
+++ IMU.h
@@ -1,7 +1,7 @@
 // for board 9-Axis IMU L3GD20 LSM303D Module 9DOF Compass Acceleration Gyroscope For Arduino (lies about the gyro being used)
 // with LSM303DLHC accelerometer/magnetometer and L3G4200D gyro
 
-#include <Wire.h>
+#include <Arduino.h>
 
 // i2c addresses for accelerometer, magnetometer, and gyro
 #define ACC_I2C_ADD 0b0011001
@@ -15,6 +15,7 @@
 
 // configuration for accelerometer
 #define ACC_DATA_RATE_1344_5376HZ 0b1001
+#define ACC_NORMAL_POWER_MODE 0b0
 #define ACC_LOW_POWER_MODE 0b1
 #define ACC_SCALE_2g 0b00 // g is gravitational acceleration
 
@@ -34,11 +35,14 @@ class IMU
 		// factors for converting readings to 
 		float acc_factor;
 		float gyro_factor;
+		uint8_t gyr_status;
+		uint8_t acc_status;
+		void init_accelerometer(float local_g_acc, uint8_t mode);
+		void init_gyro();
 	public:
-		IMU();
+		IMU(float local_g_acc = 9.8);
 		float xa, ya, za; // xyz accelerations (in meters per second^2)
 		float xw, yw, zw; // xyz angular velocities (in degrees per second)
-		void init_sensors(float local_g_acc = 9.8);
 		bool read_accelerometer(); // returns whether reading is new or a repeat of previous reading
 		bool read_gyro(); // returns whether reading is new or a repeat of previous reading
-}
+};
